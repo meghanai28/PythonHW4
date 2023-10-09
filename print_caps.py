2cc95f2db71a38d9ea2a3761ccb1d4366eb8267a
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2023-10-09 12:09:28 -0700

    Update print_caps.py

--- print_caps.py
+++ print_caps.py
@@ -1,12 +1,5 @@
 def allcaps (f):
     def inner():
-        val = f()
-        if(isinstance(val,str)):
-            print(val.upper())
-        if(isinstance(val,list)):
-            for i in range(len(val)):
-                if(isinstance(val[i],str)):
-                    val[i] = val[i].upper()
-            print(val)
+        print(f().lower())
    
     return inner
