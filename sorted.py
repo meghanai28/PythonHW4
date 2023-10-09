27578a4357a6af0040b11505fc6a8d39d4785dd4
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2023-10-09 11:17:23 -0700

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -3,6 +3,6 @@ def sort_dictionary(dict):
     key.sort()
     ans = []
     for i in range (len(key)-1,-1,-1):
-        ans.append("(" + key[i] + ","+ dict[[key]][0] + ")")
+        ans.append("(" + key[i] + ","+ dict[[key[i]]][0] + ")")
         
     return ans
