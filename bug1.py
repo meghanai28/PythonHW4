6817837c50635d75d4997b13cf6450e16d8ae9be
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2023-10-09 09:31:09 -0700

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,18 +1,20 @@
 class Base:
-    def __init__(self, x, y, size):
+    
+    def __init__(self, x, y, size, shape):
         self.x = x
         self.y = y
         self.size = size
+        self.shp = shape
 
-    def shape(self, string):
-        return string
+    def shape(self):
+        return self.shp
         
     def draw(self, string):
         return string
 
 class Circle(Base):
     def __init__(self, x, y, size):
-        super().__init__(x, y, size)
+        super().__init__(x, y, size, "This is a circle")
 
     def draw(self):
         return f"""({self.x}, {self.y})\n{self.size}
@@ -32,4 +34,4 @@ class Circle(Base):
 
 
     def shape(self):
-        return super().shape("This is a circle")
+        return super().shape()
