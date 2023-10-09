d34a82539d4d62fa67361a4aea8aafc4df17d59d
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2023-10-09 09:51:57 -0700

    Create ladder.py

---
+++ ladder.py
@@ -0,0 +1,12 @@
+def climbStairs(n):
+        
+        if(n<1 or n>25):
+            raise ValueError()
+        dp = {}
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp [i-2]
+    
+        return dp[n]
