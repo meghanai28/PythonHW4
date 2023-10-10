bf4d3b1a2de14506b9d4f8db8ca6f27928ecdd33
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2023-10-09 18:58:22 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,13 +1,13 @@
 def merge_list(l1, l2):
     if(not isinstance(l1,list) or not isinstance(l2,list)):
-        raise TypeError()
+        raise TypeError("inputs should be lists")
     
     l1.extend(l2)
     
     for i in range(len(l1)):
         for j in range(i,0,-1):
             if(not isinstance(l1[j],int)):
-                raise TypeError()
+                raise TypeError("all vals must be ints")
             if (l1[j] < l1[j-1]):
                 temp = l1[j]
                 l1[j] = l1[j-1]
